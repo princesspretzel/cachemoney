387238a5a07e2e0964f2294d09bf7c195fc05899
princesspretzel <brittanga@gmail.com>
2016-07-28 00:01:02 -0400

    set up one route

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	mux "github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/cache", CacheHandler).Methods("GET")
+
+	http.Handle("/", router)
+
+	fmt.Println("Handling it.")
+	http.ListenAndServe(":8000", router)
+
+}
+
+func CacheHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Cache & release"))
+}
